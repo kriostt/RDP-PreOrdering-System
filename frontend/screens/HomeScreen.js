146334495d193c8d18892d36abef531693de38b1
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-09 12:09:06 -0600

    Update HomeScreen

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -76,7 +76,7 @@ export default function HomeScreen() {
       <View className="flex-row items-center space-x-2 px-4 pb-2">
         <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
           <Icon.Search height="25" width="25" stroke="gray" />
-          <TextInput placeholder="Search menu items" className="ml-2 flex-1" />
+          <TextInput placeholder="Search" className="ml-2 flex-1" />
           <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
             <Icon.MapPin height="20" width="20" stroke="gray" />
             <Text className="text-gray-600">Red Deer, AB</Text>
