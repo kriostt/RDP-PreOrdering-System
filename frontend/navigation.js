a3c54e723b10c59098a046d919aae882fd3d5c4f
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-10 01:49:09 -0600

    Added AuthContext

--- frontend/navigation.js
+++ frontend/navigation.js
@@ -10,6 +10,7 @@ import ItemScreen from "./screens/ItemScreen";
 import CartScreen from "./screens/CartScreen";
 import SuccessfulMessageScreen from "./screens/SuccessfulMessageScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import { AuthProvider } from "./contexts/AuthContext";
 
 // create a stack navigator
 const Stack = createNativeStackNavigator();
@@ -17,30 +18,32 @@ const Stack = createNativeStackNavigator();
 // function for rendering navigation stack
 export default function Navigation() {
   return (
-    // navigation container
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        {/* ---Stack screens--- */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Item" component={ItemScreen} />
-        <Stack.Screen
-          name="Cart"
-          options={{ presentation: "modal" }}
-          component={CartScreen}
-        />
-        <Stack.Screen
-          name="SuccessfulMessage"
-          options={{ presentation: "fullScreenModal" }}
-          component={SuccessfulMessageScreen}
-        />
-        <Stack.Screen name="OrderHistory" component={OrderHistoryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      {/* navigation container */}
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          {/* ---Stack screens--- */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Item" component={ItemScreen} />
+          <Stack.Screen
+            name="Cart"
+            options={{ presentation: "modal" }}
+            component={CartScreen}
+          />
+          <Stack.Screen
+            name="SuccessfulMessage"
+            options={{ presentation: "fullScreenModal" }}
+            component={SuccessfulMessageScreen}
+          />
+          <Stack.Screen name="OrderHistory" component={OrderHistoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
