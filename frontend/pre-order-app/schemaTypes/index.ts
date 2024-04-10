c6135adb4632760758b5d98a1e4a22174fdeec8b
devnexuss <129311516+devnexuss@users.noreply.github.com>
2024-04-09 18:00:48 -0600

    Added user schema

--- frontend/pre-order-app/schemaTypes/index.ts
+++ frontend/pre-order-app/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import category from './category'
 import featured from './featured'
 import dish from './dish'
 import featuredItems from './featuredItems'
+import user from './user'
 
-export const schemaTypes = [category, dish, featured, featuredItems]
+export const schemaTypes = [category, dish, featured, featuredItems, user]
