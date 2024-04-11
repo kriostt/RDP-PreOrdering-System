da20b5eefab99784383a973fc78933548b8c2402
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-10 22:02:29 -0600

    Added placeholder admin screen

---
+++ frontend/screens/AdminScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AdminScreen = () => {
+  return (
+    <View>
+      <Text>Welcome admin!</Text>
+    </View>
+  );
+};
+
+export default AdminScreen;
