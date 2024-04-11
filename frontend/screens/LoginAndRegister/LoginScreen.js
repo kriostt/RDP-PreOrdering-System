d1d7fbfe983118b9edda95908d0ace0e5c443a7a
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-11 00:47:05 -0600

    Added keyboard avoiding view

--- frontend/screens/LoginAndRegister/LoginScreen.js
+++ frontend/screens/LoginAndRegister/LoginScreen.js
@@ -6,6 +6,8 @@ const {
   TouchableOpacity,
   ScrollView,
   Alert,
+  KeyboardAvoidingView,
+  Keyboard,
 } = require("react-native");
 import { useNavigation } from "@react-navigation/native";
 import styles from "./style";
@@ -19,6 +21,7 @@ function LoginPage() {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   function handleSubmit() {
     console.log(email, password);
@@ -31,87 +34,104 @@ function LoginPage() {
       console.log(res.data);
       if (res.data.status == "ok") {
         Alert.alert("Logged In Successfully!");
-        // AsyncStorage.setItem("token", res.data.data);
-        // AsyncStorage.setItem("isLoggedIn", JSON.stringify(true));
-        // AsyncStorage.setItem("userType", res.data.userType);
         navigation.navigate("Home");
       }
     });
-  }
-  // async function getData() {
-  //   const data = await AsyncStorage.getItem("isLoggedIn");
 
-  //   console.log(data, "at app.jsx");
-  // }
-  // useEffect(() => {
-  //   getData();
-  //   console.log("Hii");
-  // }, []);
+    Keyboard.dismiss();
+  }
 
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      keyboardShouldPersistTaps={"always"}
-    >
-      <View style={styles.mainContainer}>
-        {/* login screen image */}
-        <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={require("../../assets/images/login.jpg")}
-          />
-        </View>
-
-        <View style={styles.loginContainer}>
-          {/* title  */}
-          <Text style={styles.text_header}>Login</Text>
-
-          {/* email input */}
-          <View style={styles.action}>
-            <FontAwesome
-              name="user-o"
-              color="#003366"
-              style={styles.smallIcon}
-            />
-            <TextInput
-              placeholder="Email"
-              style={styles.textInput}
-              onChange={(e) => setEmail(e.nativeEvent.text)}
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps={"always"}
+      >
+        <View style={styles.mainContainer}>
+          {/* login screen image */}
+          <View style={styles.logoContainer}>
+            <Image
+              style={styles.logo}
+              source={require("../../assets/images/login.jpg")}
             />
           </View>
 
-          {/* password input */}
-          <View style={styles.action}>
-            <FontAwesome name="lock" color="#003366" style={styles.smallIcon} />
-            <TextInput
-              placeholder="Password"
-              style={styles.textInput}
-              onChange={(e) => setPassword(e.nativeEvent.text)}
-            />
-          </View>
-        </View>
+          <View style={styles.loginContainer}>
+            {/* title  */}
+            <Text style={styles.text_header}>Login</Text>
 
-        {/* login button */}
-        <View style={styles.button}>
-          <TouchableOpacity style={styles.inBut} onPress={() => handleSubmit()}>
-            <View>
-              <Text style={styles.textSign}>Log in</Text>
+            {/* email input */}
+            <View style={styles.action}>
+              <FontAwesome
+                name="user-o"
+                color="#003366"
+                style={styles.smallIcon}
+              />
+              <TextInput
+                placeholder="Email"
+                style={styles.textInput}
+                onChange={(e) => setEmail(e.nativeEvent.text)}
+              />
             </View>
-          </TouchableOpacity>
 
-          {/* link to navgigate to the register page */}
-          <View style={{ padding: 15 }}>
-            <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-              <Text
-                style={{ fontSize: 14, fontWeight: "bold", color: "#007bff" }}
-              >
-                Don't have an account?
-              </Text>
+            {/* password input */}
+            <View style={styles.action}>
+              <FontAwesome
+                name="lock"
+                color="#003366"
+                style={styles.smallIcon}
+              />
+              <TextInput
+                placeholder="Password"
+                style={styles.textInput}
+                onChange={(e) => setPassword(e.nativeEvent.text)}
+                secureTextEntry={showPassword}
+              />
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                {password.length < 1 ? null : !showPassword ? (
+                  <Feather
+                    name="eye-off"
+                    style={{ marginRight: -10, paddingBottom: 12 }}
+                    color="#003366"
+                    size={23}
+                  />
+                ) : (
+                  <Feather
+                    name="eye"
+                    style={{ marginRight: -10 }}
+                    color="#003366"
+                    size={23}
+                  />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* login button */}
+          <View style={styles.button}>
+            <TouchableOpacity
+              style={styles.inBut}
+              onPress={() => handleSubmit()}
+            >
+              <View>
+                <Text style={styles.textSign}>Log in</Text>
+              </View>
             </TouchableOpacity>
+
+            {/* link to navgigate to the register page */}
+            <View style={{ padding: 15 }}>
+              <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                <Text
+                  style={{ fontSize: 14, fontWeight: "bold", color: "#007bff" }}
+                >
+                  Don't have an account?
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 export default LoginPage;
