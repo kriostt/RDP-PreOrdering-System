52202df5f19585879b3438454ef4f2bcdb7a03e9
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-10 22:02:58 -0600

    Removed AuthProvider

--- frontend/navigation.js
+++ frontend/navigation.js
@@ -3,14 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 // import screens
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginAndRegister/LoginScreen";
+import RegisterScreen from "./screens/LoginAndRegister/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ItemScreen from "./screens/ItemScreen";
 import CartScreen from "./screens/CartScreen";
 import SuccessfulMessageScreen from "./screens/SuccessfulMessageScreen";
-import OrderHistoryScreen from "./screens/OrderHistoryScreen";
-import { AuthProvider } from "./contexts/AuthContext";
+import AdminScreen from "./screens/AdminScreen";
 
 // create a stack navigator
 const Stack = createNativeStackNavigator();
@@ -18,32 +17,30 @@ const Stack = createNativeStackNavigator();
 // function for rendering navigation stack
 export default function Navigation() {
   return (
-    <AuthProvider>
-      {/* navigation container */}
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          {/* ---Stack screens--- */}
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Item" component={ItemScreen} />
-          <Stack.Screen
-            name="Cart"
-            options={{ presentation: "modal" }}
-            component={CartScreen}
-          />
-          <Stack.Screen
-            name="SuccessfulMessage"
-            options={{ presentation: "fullScreenModal" }}
-            component={SuccessfulMessageScreen}
-          />
-          <Stack.Screen name="OrderHistory" component={OrderHistoryScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    // navigation container
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {/* ---Stack screens--- */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Admin" component={AdminScreen} />
+        <Stack.Screen name="Item" component={ItemScreen} />
+        <Stack.Screen
+          name="Cart"
+          options={{ presentation: "modal" }}
+          component={CartScreen}
+        />
+        <Stack.Screen
+          name="SuccessfulMessage"
+          options={{ presentation: "fullScreenModal" }}
+          component={SuccessfulMessageScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
